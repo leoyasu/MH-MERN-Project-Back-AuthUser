e71cf72d9a0da2163d43ee5d6ea3f90e865bcd46
LeandroYasutake <leandroyasutake@gmail.com>
2023-11-09 18:33:22 -0300

    joi added for sign in

--- config/validator.js
+++ config/validator.js
@@ -1,6 +1,6 @@
 const joi = require("joi");
 
-const validator = (req, res, next) => {
+const validatorSignUp = (req, res, next) => {
     const schema = joi.object({
         fullName: joi.string().required(),
         email: joi.string()
@@ -34,4 +34,39 @@ const validator = (req, res, next) => {
     }
     next()
 }
-module.exports = validator
+module.exports = validatorSignUp
+
+const validatorSignIn = (req, res, next) => {
+    const schema = joi.object({
+        email: joi.string()
+            .email({ minDomainSegments: 2 })
+            .required()
+            .messages({
+                'string.email': 'Wrong Email format'
+            }),
+        password: joi.string()
+            .min(8)
+            .max(30)
+            .pattern(new RegExp(/(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])/))
+            .required()
+            .messages({
+                'string.min': 'The password must have at least 8 characters.',
+                'string.max': 'The password must have a maximum of 30 characters',
+                'string.pattern.base': 'The password must contain at least one uppercase character, one lowercase character, and a number'
+            }),
+        from: joi.string().required(),
+        application: joi.string().required()
+    });
+
+    const validation = schema.validate(req.body.formData, { abortEarly: false })
+    
+    if (validation.error) {
+        return res.json({
+            success: false,
+            from: "validator",
+            message: validation.error.details
+        })
+    }
+    next()
+}
+module.exports = validatorSignIn
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const userRouter = express.Router()
 const userController = require('../controllers/usersController')
-const validator = require('../config/validator')
+const validatorSignUp = require('../config/validator')
+const validatorSignIn = require('../config/validator')
 const { signUp, signIn } = userController
 
-userRouter.route('/users/Auth/signUp').post(validator,signUp)
-userRouter.route('/users/Auth/signIn').post(signIn)
+userRouter.route('/users/Auth/signUp').post(validatorSignUp,signUp)
+userRouter.route('/users/Auth/signIn').post(validatorSignIn,signIn)
 
 module.exports = userRouter
