156c30aaae072444d12417369c5f165d2c1f7761
LeandroYasutake <leandroyasutake@gmail.com>
2023-11-17 19:03:06 -0300

    agregado solo enviar mail

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,10 +1,68 @@
 const Users = require('../models/userModel')
 const bcryptjs = require('bcryptjs')
+const crypto = require('crypto')
+const nodemailer = require('nodemailer')
+const { google } = require('googleapis')
+const Oauth2 = google.auth.OAuth2
+
+const sendEmail = async (email, uniqueString) => {
+
+    const myOauth2Client = new Oauth2(
+        process.env.GOOGLE_CLIENTID,
+        process.env.GOOGLE_SECRET,
+        "https://developers.google.com/oauthplayground"
+    )
+
+    myOauth2Client.setCredentials({refresh_token:process.env.GOOGLE_REFRESHTOKEN});
+
+    const accessToken = myOauth2Client.getAccessToken;
+
+    const transporter = nodemailer.createTransport({
+
+        service: "gmail",
+        auth: {
+            user: "leandrodeveloper435@gmail.com",
+            type: "Oauth2",
+            clientId: process.env.GOOGLE_CLIENTID,
+            clientSecret: process.env.GOOGLE_SECRET,
+            refreshToken: process.env.GOOGLE_REFRESHTOKEN,
+            accessToken: accessToken
+        },
+        tls:{
+            rejectUnauthorized: false
+        }
+    })
+
+    const emailOptions = {
+        from: "leandrodeveloper435@gmail.com",
+        to: email,
+        subject: "Email verification",
+        html:`
+            <div> 
+                <h1>
+                    <a href="http://localhost:3000/api/users/verifyEmail/${uniqueString}">
+                        Click to verify your email
+                    </a>
+                </h1>
+            </div>
+        `
+    }
+    
+    transporter.sendMail(emailOptions, function(error,response){
+        if (error){
+            console.log(error)
+        } else {
+            console.log("email sent")
+        }
+    })
+}
 
 const userController = {
     signUp: async (req, res) => {
         const { fullName, email, password, from, application } = req.body.userData;
         const hashedPassword = bcryptjs.hashSync(password, 10);
+        const emailVerify = false;
+        const uniqueString = crypto.randomBytes(15).toString('hex')
         try {
             const userExist = await Users.findOne({ email })
 
@@ -13,17 +71,28 @@ const userController = {
                     res.json({
                         success: false,
                         from: { from },
-                        message: "El Sign Up ya fue realizado mediante " + from + ", por favor realizar Sign in"
+                        message: "The Sign Up has already been done using " + from + ", please sign in"
                     })
                 } else {
                     userExist.from.push(from)
                     userExist.password.push(hashedPassword)
-                    await userExist.save()
-                    res.json({
-                        success: true,
-                        from: from,
-                        message: "Se agregó " + from + " como medio a tus metodos para realizar sign in"
-                    })
+
+                    if (from !== 'sign-up-form') {
+                        userExist.emailVerify = true;
+                        await userExist.save()
+                        res.json({
+                            success: true,
+                            from: from,
+                            message: "Added " + from + " to your sign in methods and the email was verified"
+                        })
+                    } else {
+                        await userExist.save()
+                        res.json({
+                            success: true,
+                            from: from,
+                            message: "Added " + from + " to your sign in methods"
+                        })
+                    }
                 }
             } else {
                 const newUser = new Users({
@@ -31,32 +100,47 @@ const userController = {
                     email,
                     password: [hashedPassword],
                     from: [from],
-                    application
+                    application,
+                    emailVerify,
+                    uniqueString
                 })
 
-                await newUser.save()
-                res.json({
-                    success: true,
-                    from: from,
-                    message: "El usuario ha sido creado y agregamos como metodo de logeo: " + from
-                })
+                if (from === 'sign-up-form') {
+                    await newUser.save()
+
+                    sendEmail(newUser.email, newUser.uniqueString)
+
+                    res.json({
+                        success: true,
+                        from: from,
+                        message: "Check your email to validate your account and complete the Sign Up"
+                    })
+                } else {
+                    newUser.emailVerify = true;
+                    await newUser.save()
+                    res.json({
+                        success: true,
+                        from: from,
+                        message: "The user has been created and we added it as a login method: " + from
+                    })
+                }
             }
         } catch (error) {
             console.log(error)
-            res.json({ sucess: false, message: "Algo ha salido mal" })
+            res.json({ sucess: false, message: "Something went wrong" })
         }
     },
     signIn: async (req, res) => {
         const { email, password, from } = req.body.formData
 
         try {
-            const user = await Users.findOne({email})
+            const user = await Users.findOne({ email })
             const matchPassword = user.password.filter(pass => bcryptjs.compareSync(password, pass))
             if (!user) {
                 res.json({
                     success: false,
                     from: from,
-                    message: "Sign In fallido, No has realizado Sign Up con este email",
+                    message: "Sign In failed, You have not signed Up with this email",
                 })
             } else {
                 const dataUser = {
@@ -70,7 +154,7 @@ const userController = {
                         res.json({
                             success: true,
                             from: from,
-                            message: "Sign In realizado",
+                            message: "Sign In successful, welcome again: " + dataUser.fullName,
                             response: dataUser
                         })
                     } else {
@@ -82,7 +166,7 @@ const userController = {
                         res.json({
                             success: true,
                             from: from,
-                            message: "No contabas con este ingreso: " + from + ". Se ha agregado",
+                            message: "You did not have this sign in method: " + from + ". It has been added",
                             response: dataUser
                         })
                     }
@@ -91,14 +175,14 @@ const userController = {
                         res.json({
                             success: true,
                             from: from,
-                            message: "Sign In realizado",
+                            message: "Sign In successful, welcome again: " + dataUser.fullName,
                             response: dataUser
                         })
                     } else {
                         res.json({
                             success: false,
                             from: from,
-                            message: "El user o password no coinciden",
+                            message: "The username or password do not match",
                         })
                     }
                 }
@@ -108,7 +192,7 @@ const userController = {
             res.json({
                 success: false,
                 from: from,
-                message: "Sign In fallido, algo salió mal. Reintentar en unos minutos!  " + from,
+                message: "Sign In failed, something went wrong. Retry in a few minutes!",
                 response: error
             })
         }
